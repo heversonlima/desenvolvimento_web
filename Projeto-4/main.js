4b17764ed2072b3f24253cb47f9fc5fee0b4ca2e
Heverson Lima <81335621+heversonlima@users.noreply.github.com>
2023-09-29 20:18:53 -0300

    dinamic information

--- Projeto-4/main.js
+++ Projeto-4/main.js
@@ -1,10 +1,22 @@
-/*aplicando ação no botaõ de abrir menu*/ 
-document.querySelector("#open-nav-menu").addEventListener("click", function(){
-    document.querySelector("header nav .wrapper").classList.add("nav-open")
-})
+//aplicando ação no botaõ de abrir menu
+document.querySelector("#open-nav-menu").addEventListener("click", function () {
+  document.querySelector("header nav .wrapper").classList.add("nav-open");
+});
 /*aplicando ação no botão de fechar botão*/
-document.querySelector("#close-nav-menu").addEventListener("click", function(){
-    document.querySelector("header nav .wrapper").classList.remove("nav-open")
-})
+document
+  .querySelector("#close-nav-menu")
+  .addEventListener("click", function () {
+    document.querySelector("header nav .wrapper").classList.remove("nav-open");
+  });
 
+//Previsão do tempo
 
+const saudacoes = "Bom dia!";
+const previsaoTempo = "ensolarado";
+const userLocal = "Cametá";
+let temperatura = 29.97673;
+let condicoesTexto = `A previsao é de um dia ${previsaoTempo} em 
+${userLocal} com a temperatura de ${temperatura.toFixed(1)}C°`;
+
+document.querySelector("#greeting").innerHTML = saudacoes;
+document.querySelector("p#weather").innerHTML = condicoesTexto
